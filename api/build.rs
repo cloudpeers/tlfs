d9fced2a01fea656cde9c42cc11b8ecd8ddf9bbc
David Craven <david@craven.ch>
2021-12-03 19:36:35 +0100

    Implement api.

--- api/build.rs
+++ api/build.rs
@@ -4,12 +4,15 @@ use std::path::PathBuf;
 fn main() {
     let dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let path = dir.join("tlfs.rsh");
-    println!("cargo:rerun-if-changed={}", path.as_path().to_str().unwrap());
+    println!(
+        "cargo:rerun-if-changed={}",
+        path.as_path().to_str().unwrap()
+    );
     let ffigen = FfiGen::new(&path).unwrap();
-    let dart = dir.join("dart").join("lib").join("bindings.dart");
-    ffigen.generate_dart(dart, "tlfs-api").unwrap();
+    let dart = dir.join("dart").join("lib").join("tlfs.dart");
+    ffigen.generate_dart(dart, "tlfs", "tlfs").unwrap();
     let js = dir.join("js").join("bindings.js");
     ffigen.generate_js(js).unwrap();
-    let ts = dir.join("js").join("bindings.d.ts");
-    ffigen.generate_ts(ts).unwrap();
+    //let ts = dir.join("js").join("bindings.d.ts");
+    //ffigen.generate_ts(ts).unwrap();
 }
